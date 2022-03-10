3e61867fe6a51a22f036c4d746544096a593fec4
techiew <marius.tw@hotmail.no>
2022-03-10 19:32:44 +0100

    Credits

--- OfflineLauncher/Main.cpp
+++ OfflineLauncher/Main.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-	SetEnvironmentVariableA("SteamAppId", "1245620");
+	SetEnvironmentVariableA("SteamAppId", "1245620"); // Thanks to LukeYui for this
 	ShellExecute(NULL, "open", "eldenring.exe", NULL, NULL, SW_SHOWDEFAULT);
 }
